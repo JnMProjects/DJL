1a423c11088b5cdca3da8c46196afeeca815579a
Turbobot <turbobot@vercel.com>
2024-06-05 20:32:35 +0200

    feat(create-turbo): apply official-starter transform

---
+++ apps/testsite/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Item } from "@derjacklive/djlui/layout"
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Item >
+        <h1 className="text-4xl font-bold">Hello, world!</h1>
+      </Item>
+    </div>
+  )
+}
