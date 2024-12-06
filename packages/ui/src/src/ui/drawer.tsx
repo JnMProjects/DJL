44cfb20bdf2cf082056e451e9656845319e6b7d5
DJL - Jack <71508487+JackatDJL@users.noreply.github.com>
2024-12-06 22:24:58 +0100

    Refactor tsconfig paths in UI package

--- packages/ui/src/src/ui/drawer.tsx
+++ packages/ui/src/src/ui/drawer.tsx
@@ -29,7 +29,7 @@ const DrawerTrigger = React.forwardRef<
     <Button>{children}</Button>
   </DrawerPrimitive.Trigger>
 ));
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access -- Should be inferred by the import
+ 
 DrawerTrigger.displayName = DrawerPrimitive.Trigger.displayName;
 
 const DrawerPortal = DrawerPrimitive.Portal;
@@ -57,7 +57,7 @@ const DrawerOverlay = React.forwardRef<
     {...props}
   />
 ));
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access -- Should be inferred by the import
+ 
 DrawerOverlay.displayName = DrawerPrimitive.Overlay.displayName;
 
 const DrawerContent = React.forwardRef<
@@ -134,7 +134,7 @@ const DrawerTitle = React.forwardRef<
     {...props}
   />
 ));
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access -- Should be inferred by the import
+ 
 DrawerTitle.displayName = DrawerPrimitive.Title.displayName;
 
 const DrawerDescription = React.forwardRef<
@@ -147,7 +147,7 @@ const DrawerDescription = React.forwardRef<
     {...props}
   />
 ));
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access -- Should be inferred by the import
+ 
 DrawerDescription.displayName = DrawerPrimitive.Description.displayName;
 
 export {
