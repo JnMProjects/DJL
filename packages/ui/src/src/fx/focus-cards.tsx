6d631ee56c80f693b7a2ae9c994bb87960a3df48
DJL - Jack <71508487+JackatDJL@users.noreply.github.com>
2024-12-14 17:27:58 +0100

    Wrote more tests

--- packages/ui/src/src/fx/focus-cards.tsx
+++ packages/ui/src/src/fx/focus-cards.tsx
@@ -24,8 +24,9 @@ export const Card = memo(
     <div
       className={cn(
         "rounded-lg relative bg-gray-100 dark:bg-neutral-900 overflow-hidden h-60 md:h-96 w-full transition-all duration-300 ease-out",
-        hovered !== null && hovered !== index && "blur-sm scale-[0.98]",
+        hovered !== null && hovered !== index && "blur-sm scale-[0.98]"
       )}
+      data-testid={`focus-cards`} // Added testid for each card
       onMouseEnter={() => {
         setHovered(index);
       }}
@@ -44,7 +45,7 @@ export const Card = memo(
       <div
         className={cn(
           "absolute inset-0 bg-black/50 flex items-end py-8 px-4 transition-opacity duration-300",
-          hovered === index ? "opacity-100" : "opacity-0",
+          hovered === index ? "opacity-100" : "opacity-0"
         )}
       >
         <div className="text-xl md:text-2xl font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-200">
@@ -52,7 +53,7 @@ export const Card = memo(
         </div>
       </div>
     </div>
-  ),
+  )
 );
 
 Card.displayName = "Card";
@@ -68,7 +69,12 @@ export function FocusCards({ cards }: { cards: Card[] }): React.JSX.Element {
   const [hovered, setHovered] = useState<number | null>(null);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-5xl mx-auto md:px-8 w-full">
+    <div
+      className="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-5xl mx-auto md:px-8 w-full"
+      data-testid="focus-card-wrapper"
+    >
+      {" "}
+      // Added testid for the wrapping div
       {cards.map((card, index) => (
         <Card
           card={card}
